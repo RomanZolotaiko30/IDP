5338e8a2eae7d9a207c3bd219b7835adae69746d
Roman Zolotaiko <roman.zolotaiko@redactus.co.uk>
2021-10-06 19:33:14 +0300

    Worker

---
+++ Worker/factorial-helper.js
@@ -0,0 +1,7 @@
+export default function calculateFactorial(number) {
+    if (number == 0 || number == 1) {
+        return 1;
+    } else {
+        return number * calculateFactorial(number - 1);
+    }
+}
